3dc2113aaa97ecb3284e8d5007f2ccd7c3c53645
Jan Tvrdik <jan.tvrdik@gmail.com>
2013-07-01 23:17:18 +0200

    FIX: add EditorModel

---
+++ tests/bootstrap.php
@@ -0,0 +1,41 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (!class_exists('Tester\Assert')) {
+	echo "Install Nette Tester using `composer update --dev`\n";
+	exit(1);
+}
+
+Tester\Helpers::setup();
+
+function id($val)
+{
+	return $val;
+}
+
+function before(\Closure $function = NULL)
+{
+	static $val;
+	if (!func_num_args()) {
+		return ($val ? $val() : NULL);
+	}
+	$val = $function;
+}
+
+function test(\Closure $function)
+{
+	before();
+	$function();
+}
+
+$configurator = new Nette\Configurator;
+$configurator->setDebugMode(FALSE);
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__ . '/../app')
+	->register();
+
+$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
+$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+return $configurator->createContainer();
