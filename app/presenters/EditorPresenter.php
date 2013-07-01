00e76409a66325223120b5e26b5647ec127441a2
Jan Tvrdik <jan.tvrdik@gmail.com>
2013-07-01 23:16:49 +0200

    EditorPresenter: added openPageForm

--- app/presenters/EditorPresenter.php
+++ app/presenters/EditorPresenter.php
@@ -1,9 +1,29 @@
 <?php
 namespace App;
 
+use Nette\Application\UI;
+
 
 final class EditorPresenter extends BasePresenter
 {
+
+	/**
+	 * @var EditorModel
+	 * @inject
+	 */
+	public $editorModel;
+
+	public function renderDefault($branch, $path)
+	{
+		if ($branch && $path) {
+			try {
+				$this['editor']->content = $this->editorModel->loadPageContent($branch, $path);
+			} catch (\Github\Exception\RuntimeException $e) {
+				if ($e->getCode() === 404) $this->error();
+				throw $e;
+			}
+		}
+	}
 
 	/**
 	 * @return LiveTexyEditorControl
@@ -11,6 +31,35 @@ final class EditorPresenter extends BasePresenter
 	protected function createComponentEditor()
 	{
 		return new LiveTexyEditorControl($this->context->texyFactory);
+	}
+
+	/**
+	 * @return UI\Form
+	 */
+	protected function createComponentOpenPageForm()
+	{
+		$form = new UI\Form();
+		$form->addText('page')
+			->addRule($form::URL)
+			->setRequired();
+		$form->addSubmit('send', 'Send');
+		$form->onSuccess[] = $this->openPageFormSubmitted;
+
+		return $form;
+	}
+
+
+	/**
+	 * @param UI\Form $form
+	 */
+	public function openPageFormSubmitted(UI\Form $form)
+	{
+		$pageUrl = $form->values->page;
+		list($branch, $path) = $this->editorModel->urlToRepoPath($pageUrl);
+		$this->redirect('this', array(
+			'branch' => $branch,
+			'path' => $path,
+		));
 	}
 
 }
