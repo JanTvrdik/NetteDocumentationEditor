788f6df9658d44a91cea78a95683a6e50e6e77ef
Jan Tvrdik <jan.tvrdik@gmail.com>
2013-08-28 18:53:25 +0200

    add Google's diff-match-patch lib

---
+++ www/js/diff_match_patch.d.ts
@@ -0,0 +1,39 @@
+interface Diff
+{
+	0: number;
+	1: string;
+}
+
+interface DiffMatchPatch
+{
+	Diff_Timeout: number;
+	Diff_EditCost: number;
+	Match_Threshold: number;
+	Match_Distance: number;
+	Patch_DeleteThreshold: number;
+	Patch_Margin: number;
+	Match_MaxBits: number;
+
+	diff_main(text1: string, text2: string, opt_checklines?: bool, opt_deadline?: number): Diff[];
+	diff_commonPrefix(text1: string, text2: string): number;
+	diff_commonSuffix(text1: string, text2: string): number;
+	diff_cleanupSemantic(diffs: Diff[]);
+	diff_cleanupSemanticLossless(diffs: Diff[]);
+	diff_cleanupEfficiency(diffs: Diff[]);
+	diff_cleanupMerge(diffs: Diff[]);
+	diff_xIndex(diffs: Diff[], loc: number): number;
+	diff_prettyHtml(diffs: Diff[]);
+	diff_text1(diffs: Diff[]): string;
+	diff_text2(diffs: Diff[]): string;
+	diff_levenshtein(diffs: Diff[]): number;
+	diff_toDelta(diffs: Diff[]): string;
+	diff_fromDelta(text1: string, delta: string): Diff[];
+}
+
+declare var DIFF_DELETE: number;
+declare var DIFF_INSERT: number;
+declare var DIFF_EQUAL: number;
+
+declare var diff_match_patch: {
+	new (): DiffMatchPatch;
+};
