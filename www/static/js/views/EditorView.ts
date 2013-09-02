642c933392bc175cb3c6f304b5de5525c73a47f2
Jan Tvrdik <jan.tvrdik@gmail.com>
2013-09-02 13:04:28 +0200

    EditorView.ts: refactoring

--- www/static/js/views/EditorView.ts
+++ www/static/js/views/EditorView.ts
@@ -4,6 +4,7 @@ module LiveTexyEditor
 {
 	export class EditorView
 	{
+		private header: JQuery;
 		private main: JQuery;
 		private textarea: JQuery;
 		private preview: JQuery;
@@ -13,60 +14,80 @@ module LiveTexyEditor
 		{
 			this.initElements();
 			this.initEvents();
+			this.initModel();
 			this.initPanels();
 		}
 
 		private initElements()
 		{
+			this.header = this.container.find('.header');
 			this.main = this.container.find('.main');
-			this.textarea = this.main.find('.code textarea');
-			this.preview = this.main.find('.preview iframe');
-			this.diff = this.main.find('.diff .content');
+			this.textarea = this.main.find('.column.code textarea');
+			this.preview = this.main.find('.column.preview iframe');
+			this.diff = this.main.find('.column.diff .content');
 		}
 
 		private initEvents()
 		{
-			this.container.find('select[name=panels]').on('change', (e: JQueryEventObject) => {
-				var input = <HTMLInputElement> e.target;
-				var panels = input.value.split('+');
-				window.location.hash = '#' + input.value;
-				this.model.VisiblePanels = panels;
+			this.initHeaderEvents();
+			this.initPanelsEvents();
+			this.initDropdownEvents();
+			this.initTextareaEvents();
+			this.initModelEvents();
+		}
+
+		private initHeaderEvents()
+		{
+			this.header.find('input[name=message]').on('keydown', (e: JQueryKeyEventObject) => {
+				if (e.keyCode !== 13 /* enter */ || e.ctrlKey || e.altKey || e.shiftKey || e.metaKey) return;
+				e.preventDefault();
+				this.header.find('input[name=save]').trigger('click');
+			});
+
+			this.header.find('.status button.close').on('click', (e: JQueryEventObject) => {
+				e.preventDefault();
+				$(e.target).closest('.status').remove();
+			});
+		}
+
+		private initPanelsEvents()
+		{
+			var select = this.header.find('select[name=panels]');
+
+			select.on('change', () => {
+				var value = select.val();
+				window.location.hash = '#' + value;
+				this.model.VisiblePanels = value.split('+');
 			});
 
 			$(window).on('hashchange', () => {
 				var value = location.hash.substr(1);
-				var panels = value.split('+');
-				var select = this.container.find('select');
 				if (/^[a-z+]+$/.test(value) && select.find('option[value="' + value + '"]').length) {
-					select.val(value);
-					this.model.VisiblePanels = panels;
+					select.val(value).trigger('change');
 				}
 			});
+		}
 
-			this.container.find('input[name=message]').on('keydown', (e: JQueryKeyEventObject) => {
-				if (e.keyCode !== 13 /* enter */ || e.ctrlKey || e.altKey || e.shiftKey || e.metaKey) return;
-				e.preventDefault();
-				this.container.find('input[name=save]').trigger('click');
-			});
+		private initDropdownEvents()
+		{
+			var dropdown = this.header.find('.dropdown');
 
-			this.container.find('.dropdown button').on('click', (e: JQueryEventObject) => {
+			dropdown.find('button').on('click', (e: JQueryEventObject) => {
 				e.preventDefault();
 				e.stopImmediatePropagation();
-				$(e.target).closest('.dropdown').toggleClass('open');
+				dropdown.toggleClass('open');
 			});
 
-			this.container.on('click', this.closeDropdown.bind(this));
-
-			this.container.find('.dropdown a.fullscreen').on('click', (e) => {
+			dropdown.find('li a.fullscreen').on('click', (e: JQueryEventObject) => {
 				e.preventDefault();
 				screenfull.toggle(this.container.get(0));
 			});
 
-			this.container.find('.status button.close').on('click', (e: JQueryEventObject) => {
-				e.preventDefault();
-				$(e.target).closest('.status').remove();
-			});
+			this.container.on('click', this.closeDropdown.bind(this));
+		}
 
+		private initTextareaEvents()
+		{
 			this.textarea.on('keydown', (e: JQueryKeyEventObject) => {
 				if (e.keyCode !== 9 && e.keyCode !== 13) return; // ignore everything but tab and enter
 				if (e.ctrlKey || e.altKey || e.metaKey) return;
@@ -92,7 +113,7 @@ module LiveTexyEditor
 						else lines += '\t';
 					}
 
-					// enter
+				// enter
 				} else if (e.keyCode === 13) {
 					if (start !== end) return; // ignore enter when text is selected
 
@@ -121,14 +142,14 @@ module LiveTexyEditor
 			});
 
 			this.textarea.on('keyup', (e: JQueryKeyEventObject) => {
-				var textarea = <HTMLTextAreaElement> e.target;
-				this.model.Input = textarea.value;
+				this.model.Input = this.textarea.val();
 			});
 
-			this.textarea.on('scroll', () => {
-				this.syncIframeScrollPosition();
-			});
+			this.textarea.on('scroll', this.syncIframeScrollPosition.bind(this));
+		}
 
+		private initModelEvents()
+		{
 			this.model.on('panel:show panel:hide', (e: PanelEvent) => {
 				this.main.toggleClass(e.panel.name, e.panel.visible);
 			});
@@ -148,23 +169,29 @@ module LiveTexyEditor
 			});
 		}
 
+		private initModel()
+		{
+			this.model.OriginalContent = this.getOriginalContent();
+			this.model.Input = this.textarea.val();
+		}
+
 		private initPanels()
 		{
-			var orig = this.textarea.data('original');
-			if (orig === undefined) orig = this.textarea.val();
-
-			this.model.OriginalContent = orig;
-			this.model.Input = this.textarea.val();
-
 			// init visible panels
 			if (location.hash.length > 1) $(window).trigger('hashchange');
-			this.container.find('select[name=panels]').trigger('change');
+			this.header.find('select[name=panels]').trigger('change');
 
 			// IE preview height hotfix
 			var expectedPreviewHeight = this.main.find('.right').innerHeight();
 			if (this.preview.height() !== expectedPreviewHeight) {
 				this.preview.css('height', expectedPreviewHeight + 'px');
 			}
+		}
+
+		private getOriginalContent(): string
+		{
+			var orig = this.textarea.data('original');
+			return (orig !== undefined ? orig : this.textarea.val());
 		}
 
 		private syncIframeScrollPosition()
@@ -184,7 +211,7 @@ module LiveTexyEditor
 
 		private closeDropdown()
 		{
-			this.container.find('.dropdown.open').removeClass('open');
+			this.header.find('.dropdown.open').removeClass('open');
 		}
 	}
 }
