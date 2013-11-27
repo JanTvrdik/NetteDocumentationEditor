3eca3191d06520b37f06abaa414b58e5fdb1820c
Jan Tvrdik <jan.tvrdik@gmail.com>
2013-11-27 15:16:19 +0100

    ReviewPresenter: pass pull request author to EditorPresenter

--- app/presenters/ReviewPresenter.php
+++ app/presenters/ReviewPresenter.php
@@ -27,30 +27,34 @@ final class ReviewPresenter extends BasePresenter
 			$pr = $api->show($gh['repoOwner'], $gh['repoName'], $issueId);
 			$files = $api->files($gh['repoOwner'], $gh['repoName'], $issueId);
 
+			if ($path !== NULL) {
+				foreach ($files as $file) {
+					if ($file['filename'] === $path) {
+						$texyContent = file_get_contents($file['raw_url']);
+						break;
+					}
+				}
+				if (!isset($texyContent)) $this->error();
+
+				$commits = $api->commits($gh['repoOwner'], $gh['repoName'], $issueId);
+				if (!isset($commits[0]['commit']['author'])) $this->error();
+				$author = $commits[0]['commit']['author'];
+
+				$this->forward(new Nette\Application\Request(
+					'Editor',
+					'POST',
+					['action' => 'default', 'branch' => $pr['base']['ref'], 'path' => $path],
+					['texyContent' => $texyContent, 'authorName' => $author['name'], 'authorEmail' => $author['email']]
+				));
+			}
+
+			$this->template->pr = $pr;
+			$this->template->files = $files;
+
 		} catch (Github\Exception\RuntimeException $e) {
 			if ($e->getCode() === 404) $this->error();
 			else throw $e;
 		}
-
-		if ($path !== NULL) {
-			foreach ($files as $file) {
-				if ($file['filename'] === $path) {
-					$texyContent = file_get_contents($file['raw_url']);
-					break;
-				}
-			}
-
-			if (!isset($texyContent)) $this->error();
-			$this->forward(new Nette\Application\Request(
-				'Editor',
-				'POST',
-				['action' => 'default', 'branch' => $pr['base']['ref'], 'path' => $path],
-				['texyContent' => $texyContent]
-			));
-		}
-
-		$this->template->pr = $pr;
-		$this->template->files = $files;
 	}
 
 }
